de669e48788e56ed9327e2b54aa00d1608691255
Titi <farcasanuv@yahoo.com>
2021-03-19 17:25:50 +0200

    first commit

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -5,7 +5,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
     const authContext = useContext(AuthContext);
     const { isAuthenticated, loading } = authContext;
     return (
-        <Route {...rest } render={ props => !isAuthenticated&& !loading ? (
+        <Route {...rest } render={ props => !isAuthenticated && !loading ? (
             <Redirect to='/connexion' />
         ) : (
             <Component { ...props } />
--- server.js
+++ server.js
@@ -27,12 +27,12 @@ app.use('/api/worksite-comments', require('./routes/worksite-comments'));
 
 // Serve static assets in production
 //if(process.env.NODE_ENV === 'production'){
-    console.log('Production');
+    
     //Set static folder
     //app.use(express.static('./client/build'));
     //app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
     app.use(express.static(path.join(__dirname, "./client/build")));
-    app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
+    app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./client/build/index.html")));
 //}
 
 
